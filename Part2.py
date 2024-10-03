6225c3922170bc8d3af17ad2849d458e3e5c028d
Yuriy Bandura <bandurayv@gmail.com>
2024-10-03 13:59:50 +0300

    Add_2

---
+++ Part2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = [1, 4, 6, 7]
+y = [3, 5, 8, 10]
+
+plt.scatter(x, y)
+
+plt.xlabel("x ось")
+plt.ylabel("y ось")
+plt.title("Тестовая диаграмма")
+
+plt.show()
